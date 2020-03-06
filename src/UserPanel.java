06a4020ef0aea32b98a1bfd0f21c9fdf3f8d3463
Jessica Liao <jessica.jia.liao@gmail.com>
2020-03-06 11:01:16 -0500

    Added JavaArcade methods to UserPanel

---
+++ src/UserPanel.java
@@ -0,0 +1,11 @@
+public class UserPanel implements JavaArcade{
+    public boolean running();
+    public void startGame();
+    public String getGameName();
+    public void pauseGame();
+    public String getInstructions();
+    public String getCredits();
+    public String getHighScore();
+    public void stopGame();
+    public int getPoints();
+}
